ee3dcda0ef47a17ca11d46f13abdd353b3563319
ncutLJD <962603220@qq.com>
2018-10-24 21:18:48 +0800

    IDEA整合Github测试

---
+++ shiro-test/src/main/java/com/shiro/test/IniRealmTest.java
@@ -0,0 +1,34 @@
+package com.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IniRealmTest {
+
+    @Test
+    public void testAuthentication(){
+
+        IniRealm iniRealm  = new IniRealm("classpath:realm.ini");
+        //1. 构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(iniRealm);
+        //2. 主体提交请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("Mark","123456");
+        subject.login(token);
+
+        System.out.println("isAuthenticated:" + subject.isAuthenticated());
+
+        subject.checkRole("admin");
+
+        subject.checkPermission("user:delet");
+    }
+}
